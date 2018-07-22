370ab8858a85e0926207b248d22d750c81a93a3c
ehsan <dr.enyacc@gmail.com>
2018-07-22 20:37:46 +0430

    change db config

--- db/db_config.py
+++ db/db_config.py
@@ -3,8 +3,8 @@ import os
 
 class DatabaseConfig:
     db_string_main = 'postgresql://{}:{}@{}:{}/{}'
-    db_string = db_string_main.format(os.environ.get('PG_USER', None) or "ehsan",
-                                      os.environ.get('PG_PASS', None) or "ehsan1379",
-                                      os.environ.get('PG_HOST', None) or "localhost",
-                                      os.environ.get('PG_PORT', None) or "5432",
-                                      os.environ.get('PG_DB_NAME', None) or "twitter")
+    db_string = db_string_main.format(os.environ.get('POSTGRES_USER', None) or "postgres",
+                                      os.environ.get('POSTGRES_PASSWORD', None) or "ehsan1379",
+                                      os.environ.get('POSTGRES_HOST', None) or "localhost",
+                                      os.environ.get('POSTGRES_PORT', None) or "5432",
+                                      os.environ.get('POSTGRES_DB', None) or "admin_channel")
